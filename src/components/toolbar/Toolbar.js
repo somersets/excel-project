2b01f872096b2061cac6e509318e77e53288334f
Nikita <facelessmay@gmail.com>
2021-02-21 18:23:09 +0700

    Finish base table logic

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import { ExcelComponent } from '@core/ExcelComponent';
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Toolbar',
-      listeners: ['click']
+      listeners: [],
+      ...options
     });
   }
 
@@ -25,7 +26,7 @@ export class Toolbar extends ExcelComponent {
       </div>
 
       <div class="button">
-        <i class="material-icons">format_bold</i>
+        <i class="material-icons" id="bold">format_bold</i>
       </div>
 
       <div class="button">
@@ -36,9 +37,5 @@ export class Toolbar extends ExcelComponent {
         <i class="material-icons">format_underlined</i>
       </div>
     `
-  }
-
-  onClick(event) {
-    console.log(event.target)
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -13,6 +13,17 @@ class Dom {
     return this.$el.outerHTML.trim()
   }
 
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
+  }
+
   clear() {
     this.html('')
     return this
@@ -55,6 +66,10 @@ class Dom {
     return this.$el.querySelectorAll(selector)
   }
 
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
+
   css(styles = {}) {
     Object
         .keys(styles)
@@ -62,9 +77,33 @@ class Dom {
           this.$el.style[key] = styles[key]
         })
   }
-}
 
-$('div').html('<h1>Test</h1>').clear()
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':')
+      return {
+        row: +parsed[0],
+        col: +parsed[1]
+      }
+    }
+    return this.data.id
+  }
+
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className)
+    return this
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className)
+    return this
+  }
+}
 
 export function $(selector) {
   return new Dom(selector)
