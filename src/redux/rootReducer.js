39648baaac493dc13091e4baa038d04344b0a3e3
Nikita <facelessmay@gmail.com>
2021-02-25 20:27:20 +0700

    Routing implemented

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,6 @@
 import {
   CHANGE_TEXT, CHANGE_STYLES,
-  TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE
+  TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE, UPDATE_DATE
 } from '@/redux/types';
 
 export function rootReducer(state, action) {
@@ -32,6 +32,8 @@ export function rootReducer(state, action) {
       }
     case CHANGE_TITLE:
       return {...state, title: action.data}
+    case UPDATE_DATE:
+      return {...state, openedDate: new Date().toJSON()}
     default:
       return state
   }
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
+const webpack = require('webpack')
 
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
@@ -62,6 +63,9 @@ module.exports = {
     }),
     new ExtractCssChunks({
       filename: filename('css')
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ],
   module: {
